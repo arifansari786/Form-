30429e986ab6be6ec10c44a0f80329468ef62f59
arifansari786 <44662033+arifansari786@users.noreply.github.com>
2018-11-01 11:23:13 +0530

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,42 @@
+
+<?php
+
+$con = mysql_connect("localhost","root","");
+
+if (!$con)
+
+  {
+
+  die('Could not connect: ' . mysql_error());
+
+  }
+
+ 
+
+mysql_select_db("form", $con);
+
+ 
+
+$sql="INSERT INTO entry (firstname, lastname)
+
+VALUES
+
+('$_POST[fname]','$_POST[lname]')";
+
+ 
+
+if (!mysql_query($sql,$con))
+
+  {
+
+  die('Error: ' . mysql_error());
+
+  }
+
+echo "1 record added";
+
+ 
+
+mysql_close($con)
+
+?>
